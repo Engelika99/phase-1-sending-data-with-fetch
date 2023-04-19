a48085c9d3780565434639c980bdc8ea9bad1ae8
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-04-19 18:34:56 -0400

    sends data

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const infoData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+const message = ("Unauthorized Access");
+
+
+function submitData() {
+    const configObj = {
+    method: "Post",
+    headers: {
+ "Content-Type": "application/json",
+"Accept": "application/json",
+    },
+    body: JSON.stringify(infoData),
+};
+return fetch("http://localhost:3000/users", configObj)
+.then(res => res.json())
+.then(body => newId(body.id))
+.catch(response => "Unauthorized Access")
+
+function newId(newUser) {
+    document.getElementsByTagName("body")[0].innerHTML = newUser;
+    document.body.append(message)
+}
+}
